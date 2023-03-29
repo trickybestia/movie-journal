532ff25d56ef66905de04f23f02d6e80367bb3a8
trickybestia <trickybestia@gmail.com>
2023-03-29 07:09:15 +0300

    Suppress warning

--- src/hooks/use-dynamic-context-menu.ts
+++ src/hooks/use-dynamic-context-menu.ts
@@ -2,6 +2,7 @@ import { createContext, useContext } from "react";
 
 import { ContextMenuItemProps } from "components/ContextMenu";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const DynamicContextMenuContext = createContext((items: readonly ContextMenuItemProps[]): void => {});
 
 const useDynamicContextMenu = (): { requestContextMenuItems: (items: readonly ContextMenuItemProps[]) => void } => {
