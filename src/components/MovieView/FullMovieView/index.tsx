2120ebfa9710e5cd50c0de98a23b6d284053a9c9
trickybestia <trickybestia@gmail.com>
2023-03-29 09:03:30 +0300

    Add support for adding and removing movies in model

--- src/components/MovieView/FullMovieView/index.tsx
+++ src/components/MovieView/FullMovieView/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import useDynamicContextMenu from "hooks/use-dynamic-context-menu";
+import { newMovie } from "model/new";
 import ParentState from "parent-state";
 
 import { MovieView, Props } from "..";
@@ -8,6 +10,8 @@ import SingleMovieView from "./SingleMovieView";
 import styles from "./index.module.scss";
 
 const FullMovieView: MovieView = ({ movies, filters }: Props) => {
+  const { requestContextMenuItems } = useDynamicContextMenu();
+
   const movieViews: JSX.Element[] = [];
 
   movies.state.forEach((movie, index) => {
@@ -23,7 +27,22 @@ const FullMovieView: MovieView = ({ movies, filters }: Props) => {
 
     if (shouldRender()) {
       movieViews.push(
-        <div key={index} className={styles.SingleMovieViewWrapper}>
+        <div
+          key={index}
+          className={styles.SingleMovieViewWrapper}
+          onContextMenu={() => {
+            requestContextMenuItems([
+              {
+                name: "Удалить тайтл",
+                onClick: () => {
+                  movies.update(movies => {
+                    movies.splice(index, 1);
+                  });
+                }
+              }
+            ]);
+          }}
+        >
           <SingleMovieView
             movie={
               new ParentState(movie, newMovie =>
@@ -39,7 +58,25 @@ const FullMovieView: MovieView = ({ movies, filters }: Props) => {
     }
   });
 
-  return <div className={styles.FullMovieView}>{movieViews}</div>;
+  return (
+    <div
+      className={styles.FullMovieView}
+      onContextMenu={() => {
+        requestContextMenuItems([
+          {
+            name: "Добавить тайтл",
+            onClick: () => {
+              movies.update(movies => {
+                movies.push(newMovie());
+              });
+            }
+          }
+        ]);
+      }}
+    >
+      {movieViews}
+    </div>
+  );
 };
 
 export default FullMovieView;
--- src/model/new.ts
+++ src/model/new.ts
@@ -1,7 +1,11 @@
-import { ModelType } from "model";
+import { ModelType, MovieType } from "model";
 
 const newModel = (): ModelType => {
   return { movies: [] };
 };
 
-export { newModel };
+const newMovie = (): MovieType => {
+  return { title: "Новый тайтл", description: "", seasons: [], mainPreviewSeasonIndex: undefined, tags: [] };
+};
+
+export { newModel, newMovie };
